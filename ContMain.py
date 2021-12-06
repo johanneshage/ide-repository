994d54d79f619e7cacaf1eeac4e653dc2f1dc7a0
johanneshage <66865263+johanneshage@users.noreply.github.com>
2021-12-06 08:34:44 +0100

    Continous version: graphs now get read from "GraphenGEXF/myGraph.gexf", if no input graph specified

--- ContMain.py
+++ ContMain.py
@@ -1,11 +1,53 @@
+import networkx as nx
 import Math.cont_data as data
 from Math.ContApp import ContApp
+from shutil import copyfile
 
 
 class ContMain:
     def __init__(self, graph, u, output):
 
+        # liest Graph aus "GraphenGEXF/myGraph.gexf", falls keiner in "Math/cont_data.py" angegeben ist
+        if graph is None:
+            graph = self.import_gexf_graph()
+
         self.app = ContApp(graph, u, output)
+
+    @staticmethod
+    def import_gexf_graph():
+        """
+        Funktion zum Einlesen eines Graphen aus einer .gexf Datei
+        :return: graph: gerichteter Graph als Dictionary,
+                 posit: Knotenpositionen als passende Koordinaten
+        """
+
+        # Funktion aus 'shutil', erzeugt Kopie von 'myGraph.gexf')
+        copyfile("GraphenGEXF/myGraph.gexf", "GraphenGEXF/myGraph.gexf" + "~")
+
+        # füge Ausdruck 'defaultedgetype="directed"' zu 'myGraph.gexf' hinzu (falls noch nicht vorhanden), da sonst
+        # Kantenorientierung nicht beachtet wird
+        source = open( "GraphenGEXF/myGraph.gexf" + "~", "r")
+        gexfstring = source.read()
+        if "defaultedgetype=\"directed\"" not in gexfstring[:500]:
+            destination = open( "GraphenGEXF/myGraph.gexf", "w" )
+            print("string:", gexfstring)
+            gexfstring1, gexfstring2 = gexfstring.split("graph mode=\"static\"")
+            destination.write(gexfstring1 + "graph mode=\"static\" defaultedgetype=\"directed\"" + gexfstring2)
+            destination.close()
+        source.close()
+
+        G = nx.read_gexf("GraphenGEXF/myGraph.gexf")
+        nodes = G.nodes()
+        edges = G.edges()
+        graph = {}
+        for v in nodes:
+            graph[nodes[v]['label']] = {}
+            for m in edges:
+                if m[0] == v:
+                    cap_trav = edges[m]['label'].split("/")
+                    # speichern der Kapazität und der Reisezeit von Kante m, entnommen aus der .gexf-Datei
+                    graph[nodes[v]['label']][nodes[m[1]]['label']] = (float(cap_trav[0]), float(cap_trav[1]))
+        return graph
 
 
 def main():
@@ -26,7 +68,6 @@ def main():
                 cont_main.app.output[7]: Liste der Knoten
     """
     try:
-        graph = data.graph
         u = data.u
     except AttributeError:
         raise AttributeError('Daten unvollständig!')
@@ -34,6 +75,10 @@ def main():
         output = data.table_output
     except AttributeError:
         output = True
+    try:
+        graph = data.graph
+    except AttributeError:
+        graph = None
 
     cont_main = ContMain(graph, u, output)
     if not output:
--- Math/cont_data.py
+++ Math/cont_data.py
@@ -1,3 +1,16 @@
+"""
+Enthält die für die Erzeugung eines Objekts der Klasse "ContMain" notwendigen Parameter:
+    u: Liste aller Einflussraten. Enthält für jeden Knoten eine Liste von 2-Tupeln der Form
+       (<Startzeitpunkt>, <Einflussmenge>). <Startzeitpunkt> gibt dabei den Beginn des Einflusses von <Einflussmenge>
+       Flusseinheiten in den Knoten mit entsprechendem Index an. Dieser Einfluss bleibt bestehen bis zum
+       <Startzeitpunkt> des nächsten 2-Tupels in der selben Liste.
+    graph: Gerichteter Graph als Dictionary, falls kein Graph spezifiziert, so wird Graph aus Datei "myGraph.gexf"
+           eingelesen
+    table_output: Hat Wert 'True' oder 'False' und legt fest, ob eine 'OutputTable' erzeugt wird oder nicht. Falls
+                  nicht, so werden die darin enthaltenen Daten stattdessen als Listen ausgegeben (siehe dafür auch
+                  "ContMain"). Falls nicht spezifiziert, so wird der Standardwert 'True' verwendet.
+"""
+
 """graph = {
     's1': {'v': (1,1), 'w': (1,2)},
     's2': {'x': (2,3), 'w': (1,1)},
@@ -18,10 +31,10 @@ graph = {
     'a': {'t1': (2,4)},
     't1': {}
 }
+"""
+u = [[(0.1, 4.5), (4, 1.5), (5,0)]] + [[(0, 4.2), (1.1, 4.4), (2, 0)]] + 3 * [[]]
 
-u = [[(0.1, 4.5), (4, 1.5), (5,0)]] + [[(0, 4.2), (1.1, 4.4), (2, 0)]] + 6 * [[]]
-
-
+"""
 graph = {
     's1': {'v': (1.5,2.5), 'W': (1,3), 'u': (3,10), 't1': (1,20)},
     's2': {'W': (5,3.25), 'w': (2,3)},
@@ -37,7 +50,7 @@ u = [[(0,11.5), (1.9,2.1), (3.22,1.4), (5.2,0)]] + [[(0.9,12.1), (3.3,1), (6.4,0
 
 """
 
-graph = {
+"""graph = {
     's1': {'v': (1.1,2.5), 'W': (0.9,3), 'u': (3,10), 't1': (1,25)},
     's2': {'W': (3,3.25), 'w': (2,2.8)},
     's3': {'s1': (3,1), 'W': (2, 1.5), 'x': (1.2, 3), 'a': (2,7)},
@@ -53,6 +66,7 @@ graph = {
 }
 
 u = [[(0, 16), (0.6, 8), (1.2, 9), (4, 8.1), (7,0)]] + [[(0.2, 8), (1, 5), (1.3, 8), (5.5, 0)]] + \
-    [[(1.5, 16), (2, 14), (3, 7), (7,0), (1024,1), (1025,0)]] + 9 * [[]]
+    [[(1.5, 16), (2, 14), (3, 7), (7,0), (1024,1), (1025,0)]] + 9 * [[]]"""
+
 
 table_output = False
